245b1a734e7d14e0a62983bf7e04a82ea95e6590
marie <ematetelove@gmail.com>
2023-10-26 17:36:55 -0400

    thursday js

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ function reset(){
 
 function start(){
     console.log("startbtn clicked!");
+    shuffle();
+
     for (var i = 0; i < startdisplay.length; i++){
         startdisplay.item(i).hidden = true;
     }
@@ -30,50 +32,50 @@ function start(){
         gamedisplay.item(i).hidden = false;
     }
 
-
 }
 
 function shuffle(){
     // Math.floor(Math.random() * (max - min)) + min;
-    var img = 0;
-    var file = 0;
-    var nums = [];
+
+    console.log("shuffle begins!");
+
+    var element = 1;
+    var placed = [];
     var valid = true;
-    var cards = ["images/Killmonger.webp", "images/M'Baku.webp", "images/Namor.webp", "images/Okoye.jpeg", "images/Ramonda.avif", "images/Shuri.jpg", "images/T'Challa.webp"]
+    var card = 0;
+    var cards = [["images/Killmonger.webp", "Killmonger"], 
+    ["images/M'Baku.webp", "M'Baku"], 
+    ["images/Namor.webp", "Namor"], 
+    ["images/Okoye.jpeg", "Okoye"], 
+    ["images/Ramonda.avif", "Ramonda"], 
+    ["images/Shuri.jpg", "Shuri"], 
+    ["images/T'Challa.webp", "T'Challa"], 
+    ["images/Nakia.avif", "Nakia"]]
 
-    while (nums.length != 16) {
-        img = Math.floor(Math.random() * (7)) + 1;
+    while (placed.length != 16) {
+        /* generate random number from 0 - 7 for the cards */
+        card = Math.floor(Math.random() * (7)) + 0;
 
-         for (let i = 0; i < nums.length; i++){
-            if(nums[i] == img){
-                for (let j = i; j < nums.length; j++){
-                    if(nums[j] == img){
+        /* if card is displayed more than twice generate different card # */
+        for (let i = 0; i < placed.length; i++){
+            if(placed[i] == card){
+                for (let j = i; j < placed.length; j++){
+                    if(placed[j] == card){
                         valid = false;
                     }
                 }
 
             }
-         }
+        }
 
-         file = Math.floor(Math.random() * (max - min)) + min;
-         if (ran == 1){
-            
-         }else if (ran == 2){
+         /* add card # to array of chosen cards */
+        placed.push(card);
 
-         }else if (ran == 2){
-            
-         }else if (ran == 3){
-            
-         }else if (ran == 4){
-            
-         }else if (ran == 5){
-            
-         }else if (ran == 6){
-            
-         }else if (ran == 7){
-
-         }else {
-
-         }
+        const imgElem = document.getElementById(element);
+        imgElem.setAttribute("src", cards[card][0]);
+        const capElem = document.getElementById("p" + element);
+        capElem.innerHTML = cards[card][1];
+        element++;
+    
     }
 }
